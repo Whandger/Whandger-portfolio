14f10498a68f40215066550ad61bf3efa10936aa
Whandger <whandgerw@yahoo.com.br>
2025-11-14 16:49:37 -0300

    Delete tailwind_test directory

--- tailwind_test/js/headers.js
+++
@@ -1,9 +0,0 @@
-const header = document.getElementById('header');
-
-window.addEventListener('scroll', () => {
-  if (window.scrollY > 1) {
-    header.classList.add('shadow-md'); // adiciona a sombra
-  } else {
-    header.classList.remove('shadow-md'); // remove a sombra
-  }
-});
